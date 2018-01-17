1b9d8a12e3cb1efbe95b8ec219ab5f17cb47dc8d
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-17 17:59:12 -0500

    hur durrr

--- Search Algorithms/src/SearchAlgorithms.java
+++ Search Algorithms/src/SearchAlgorithms.java
@@ -1,16 +1,20 @@
-public class Search {
+public class SearchAlgorithms {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] test1 = {1,2,3,4,5,6,7,8,9,10};
 		int[] test2= {0,4,5,6,12,15,17,23,29};
 		System.out.println(binSearch(test1,3)); //2
 		System.out.println(linSearch(test1,3)); //2
+		
 		System.out.println(binSearch(test2,23)); //2
 		System.out.println(linSearch(test2,23)); //2
+		
 		System.out.println(binSearch(test2,25)); //2
 		System.out.println(linSearch(test2,25)); //2
+		
 		System.out.println(binSearch(test1,10)); //2
 		System.out.println(linSearch(test1,1)); //2
+		
 		System.out.println(binSearch(test1,1)); //2
 		
 	}
